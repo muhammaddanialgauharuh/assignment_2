59fd8714d9402c797dee6a5cb94dae0f6f57f103
muhammaddanialgauharuh <muhammaddanialgauharuh@gmail.com>
2023-01-03 11:42:41 +0500

    fetch the user information by their id

---
+++ profile_fetching.php
@@ -0,0 +1,33 @@
+<?php
+
+    $host="localhost";
+    $user="root";
+    $password="";
+    $db="digitalcardworld";
+
+
+    $email = $_SESSION['email'];
+    $id = $_SESSION['id'];
+
+    $db = new mysqli($host,$user,$password,$db);
+
+
+    $user_check_query = "SELECT * FROM profile WHERE id='$id' ";
+    $result = mysqli_query($db, $user_check_query);
+    $user = mysqli_fetch_assoc($result);
+
+    $dp = $user["dp"];
+    $cover = $user["cover"];
+    $display_name = $user["display_name"];
+    $job = $user["job"];
+    $bio = $user["bio"];
+    $facebook = $user["facebook"];
+    $instagram = $user["instagram"];
+    $whatsapp = $user["whatsapp"];
+    $linkedin = $user["linkedin"];
+    $mail = $user["gmail"];
+    $twitter = $user["twitter"];
+    $youtube = $user["youtube"];
+    $phone = $user["phone"];
+
+?>
